b5717a8428e72e57d6e8b6c687bc3385029f589b
Blaze Thrower <blazethrower320@gmail.com>
2025-09-10 16:02:59 -0500

    Worked on Experiment3. Making a TodoList.

---
+++ Experiments/CarsonTorrey/Experiment3/TodoListExperiment/src/main/java/coms309/Todo/Todo.java
@@ -0,0 +1,36 @@
+package coms309.Todo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Todo
+{
+    public int id;
+    public String title;
+    public String description;
+    public int dueDate;
+    public Boolean completed = false;
+
+    public Todo(int id, String title, String desc, int due, Boolean done){
+        this.id = id;
+        this.title = title;
+        this.description = desc;
+        this.dueDate = due;
+        this.completed = done;
+    }
+
+    @Override
+    public String toString() {
+        return id + " "
+                + title + " "
+                + description + " "
+                + dueDate + " "
+                + completed;
+    }
+}
---
+++ Experiments/CarsonTorrey/Experiment3/TodoListExperiment/src/main/java/coms309/Todo/TodoController.java
@@ -0,0 +1,93 @@
+package coms309.Todo;
+
+import coms309.people.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class TodoController
+{
+    HashMap<String, Todo> TodoList = new HashMap<>();
+    @GetMapping("/todo")
+    public  HashMap<String, Todo> getAllTodos() { return TodoList; }
+
+    @GetMapping("/todo/lookup/{Action}")
+    public Todo getAction(@PathVariable String Action){
+        return TodoList.get(Action);
+    }
+    @PostMapping("/todo")
+    public void CreateTodo(@RequestBody Todo todo)
+    {
+        TodoList.put(todo.title, todo);
+    }
+
+    @GetMapping("todo/lookupid/{id}")
+    public Todo LookupTodoID(@PathVariable String id)
+    {
+        Todo foundTodo = new Todo();
+        int todoID = Integer.parseInt(id);
+        for(Todo todo : TodoList.values())
+        {
+            if(todo.id == todoID)
+            {
+                foundTodo = todo;
+            }
+        }
+
+       return foundTodo;
+    }
+
+    @DeleteMapping("/todo/deleteid/{id}")
+    public void deleteID(@PathVariable String id)
+    {
+        int todoID = Integer.parseInt(id);
+        for(Todo todo : TodoList.values())
+        {
+            if(todo.id == todoID)
+            {
+                TodoList.remove(todo.title);
+                break;
+            }
+        }
+    }
+
+    @DeleteMapping("/todo/deleteTitle/{title}")
+    public void deleteTitle(@PathVariable String title)
+    {
+        TodoList.remove(title);
+    }
+    @GetMapping("/todo/getcompletedtodos")
+    public HashMap<String, Todo> GetCompletedTodos()
+    {
+        HashMap<String, Todo> completed = new HashMap<>();
+        for(Todo todo : TodoList.values())
+            if(todo.completed)
+                completed.put(todo.title, todo);
+        return completed;
+    }
+
+    @PatchMapping("/todo/complete/{id}")
+    public void completeTodo(@PathVariable String id, @RequestBody boolean completedAction)
+    {
+        int todoID = Integer.parseInt(id);
+        for(Todo todo : TodoList.values())
+        {
+            if(todo.id == todoID)
+            {
+                TodoList.get(todo.title).completed = completedAction;
+            }
+        }
+    }
+
+    @GetMapping("/todo/complete/list")
+    public HashMap<String, Todo> GetCompletedList()
+    {
+        HashMap<String, Todo> completed = new HashMap<>();
+        for(Todo todo : TodoList.values())
+            if(todo.completed)
+                completed.put(todo.title, todo);
+        return completed;
+    }
+}
+
