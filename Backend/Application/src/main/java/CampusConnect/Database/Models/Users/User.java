017c89894eeb7707aab7f10075fb2f71ca4eb3ee
Preet Patel <preetp@iastate.edu>
2025-09-29 17:21:52 -0500

    Added function to get password from a given username.

--- Backend/Application/src/main/java/CampusConnect/Database/Models/Users/User.java
+++ Backend/Application/src/main/java/CampusConnect/Database/Models/Users/User.java
@@ -16,7 +16,7 @@ public class User {
     private String major;
     private String year;
 
-    @ManyToMany
+
 
 
     public User(String firstName, String lastName, String username, String password) {
