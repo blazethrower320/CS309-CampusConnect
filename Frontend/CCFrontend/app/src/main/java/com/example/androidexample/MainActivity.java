c9f504063294fe727cc2a987d1e6a392f90afc3e
William Rossow <will512@iastate.edu>
2025-10-05 14:00:05 -0500

    Fixin conflict issue

--- Frontend/CCFrontend/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/CCFrontend/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatAct
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
