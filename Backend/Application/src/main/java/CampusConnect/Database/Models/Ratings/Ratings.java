cb971aa28c46210348f6540a79ff70cc9c47e65d
Torrey, Carson T <ctorrey@iastate.edu>
2025-10-22 19:07:18 -0500

    Figured out the issue...

--- Backend/Application/src/main/java/CampusConnect/Database/Models/Ratings/Ratings.java
+++ Backend/Application/src/main/java/CampusConnect/Database/Models/Ratings/Ratings.java
@@ -11,7 +11,7 @@ public class Ratings
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private Integer rating = 0;
+    private int rating = 0;
     private String comments = "";
 
 
