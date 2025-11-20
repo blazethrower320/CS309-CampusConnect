bce6e6b284cea88f6b46c57393e62ba64d5665cb
Preet Patel <preetp@iastate.edu>
2025-11-19 19:36:52 -0600

    testing CI/CD

--- Backend/Application/src/main/java/CampusConnect/Database/Models/Classes/ClassesController.java
+++ Backend/Application/src/main/java/CampusConnect/Database/Models/Classes/ClassesController.java
@@ -2,6 +2,7 @@ package CampusConnect.Database.Models.Classes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,6 +30,11 @@ public class ClassesController {
     public String getClassCode(@PathVariable String className){
         String currentClass = ClassesRepository.findByclassName(className).getclassCode();
         return currentClass;
+    }
+
+    @PostMapping(path = "/clses/delete/{classId}")
+    public void deleteClass(@PathVariable long classId){
+        ClassesRepository.findById(classId);
     }
 
 
