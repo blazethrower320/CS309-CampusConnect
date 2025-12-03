8ca4e43724b5dfd6ea2efa396e6145f87be4db00
Torrey, Carson T <ctorrey@iastate.edu>
2025-12-03 17:00:40 -0600

    Worked on Private Messages for Cam

--- Backend/Application/src/main/java/CampusConnect/Database/Models/Messages/MessagesController.java
+++ Backend/Application/src/main/java/CampusConnect/Database/Models/Messages/MessagesController.java
@@ -4,12 +4,18 @@ import CampusConnect.Database.Models.Ratings.Ratings;
 import CampusConnect.Database.Models.Ratings.RatingsRepository;
 import CampusConnect.Database.Models.Ratings.RatingsService;
 import CampusConnect.Database.Models.Tutors.TutorRepository;
+import CampusConnect.Database.Models.Users.User;
 import CampusConnect.Database.Models.Users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class MessagesController
@@ -17,9 +23,14 @@ public class MessagesController
     @Autowired
     MessagesRepository messagesRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping(path = "/messages")
     public List<Messages> getAllMessages() {
         return messagesRepository.findAll();
     }
+
+
 
 }
